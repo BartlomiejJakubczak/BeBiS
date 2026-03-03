c2df902a5c104bec9e6390368711dee59f8ac483
BJakubczak <bjakubczak96@gmail.com>
2026-03-03 10:48:07 +0100

    project init

---
+++ src/main/java/com/bebis/BeBiS/BeBiSApplication.java
@@ -0,0 +1,13 @@
+package com.bebis.BeBiS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBiSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeBiSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bebis/BeBiS/BeBiSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bebis.BeBiS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeBiSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
